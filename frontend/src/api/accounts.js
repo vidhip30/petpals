405a5c74ed257913609b41225cd366a5e757c6d2
Justin Sadakhom <justin.sadakhom@gmail.com>
2023-12-02 18:37:52 -0500

    Fix broken registration for shelters

--- frontend/src/api/accounts.js
+++ frontend/src/api/accounts.js
@@ -9,3 +9,15 @@ export const registerPetSeeker = async (payload) => {
 
   return response;
 };
+
+export const registerShelter = async (payload) => {
+  const response = await fetch("http://127.0.0.1:8000/accounts/shelter/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  return response;
+};
