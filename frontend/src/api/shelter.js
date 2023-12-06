d90343d6d8010324fc03a4dee1c047ad4da39ff9
Ansh2905 <anshdpatel10@gmail.com>
2023-12-06 11:36:30 -0500

    Changing bearer token

--- frontend/src/api/shelter.js
+++ frontend/src/api/shelter.js
@@ -2,7 +2,7 @@ export const createPetListing = async (payload) => {
   const response = await fetch("http://127.0.0.1:8000/shelters/petlistings/", {
     method: "POST",
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAzNzQxMzYxLCJpYXQiOjE3MDE1ODEzNjEsImp0aSI6IjU4NGMxZGVjZGZlNjQ1MjY5Yzg4NDgzYjdjZmJjODZjIiwidXNlcl9pZCI6MX0.3viiz2gssHrK9OV57yekc1Q5E7SNhF-Ru-N7nWFjRco`,
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
     },
     body: payload,
   });
@@ -14,7 +14,7 @@ export const updatePetListing = async (listingId, payload) =>{
   const response = await fetch(`http://127.0.0.1:8000/shelters/petlistings/${listingId}/`, {
     method: "PATCH",
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAzNzQxMzYxLCJpYXQiOjE3MDE1ODEzNjEsImp0aSI6IjU4NGMxZGVjZGZlNjQ1MjY5Yzg4NDgzYjdjZmJjODZjIiwidXNlcl9pZCI6MX0.3viiz2gssHrK9OV57yekc1Q5E7SNhF-Ru-N7nWFjRco`,
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
     },
     body: payload,
   });
@@ -27,7 +27,7 @@ export const getPetListing = async (listingId) =>{
   const response = await fetch(`http://127.0.0.1:8000/shelters/petlistings/${listingId}/`, {
     method: "GET",
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAzNzQxMzYxLCJpYXQiOjE3MDE1ODEzNjEsImp0aSI6IjU4NGMxZGVjZGZlNjQ1MjY5Yzg4NDgzYjdjZmJjODZjIiwidXNlcl9pZCI6MX0.3viiz2gssHrK9OV57yekc1Q5E7SNhF-Ru-N7nWFjRco`,
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
     },
   });
   console.log(response);
